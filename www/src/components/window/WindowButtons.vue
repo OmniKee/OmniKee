4ca66f6103291994d3298733d29e3eca830a98ce
Stefan Seemayer <stefan@seemayer.de>
2025-04-26 20:54:27 +0200

    mac window buttons

--- www/src/components/window/WindowButtons.vue
+++ www/src/components/window/WindowButtons.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="window-buttons" v-if="isTauri">
+  <div class="window-buttons mac q-mr-sm" v-if="isMac">
+    <div class="close" @click="onClose">
+      <img src="@/assets/window/close-mac.svg" />
+    </div>
+
+    <div class="minimize" @click="onMinimize">
+      <img src="@/assets/window/minimize-mac.svg" />
+    </div>
+
+    <div class="maximize" @click="onMaximize">
+      <img src="@/assets/window/maximize-mac.svg" />
+    </div>
+  </div>
+
+  <div class="window-buttons" v-if="isTauri && !isMac">
     <img src="@/assets/window/minimize.svg" @click="onMinimize" />
     <img src="@/assets/window/maximize.svg" @click="onMaximize" />
     <img src="@/assets/window/close.svg" @click="onClose" />
@@ -10,6 +24,7 @@
 import {getCurrentWindow} from '@tauri-apps/api/window'
 
 const isTauri = process.env.TAURI_ENV_PLATFORM !== 'web'
+const isMac = process.env.TAURI_ENV_PLATFORM === 'darwin'
 
 async function onMinimize() {
   const win = getCurrentWindow()
@@ -30,6 +45,45 @@ async function onClose() {
 </script>
 
 <style scoped lang="scss">
+.mac {
+  order: -1;
+
+  .close,
+  .minimize,
+  .maximize {
+    background: #fff;
+    border-radius: 100%;
+    width: 2vh;
+    height: 2vh;
+
+    margin: 0 6px 0 6px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+      padding: 0px;
+      opacity: 0;
+    }
+  }
+
+  .close {
+    background: #f85951;
+  }
+
+  .minimize {
+    background: #febc2e;
+  }
+
+  .maximize {
+    background: #1dad2e;
+  }
+}
+
+.mac.window-buttons:hover img {
+  opacity: 1;
+}
+
 .window-buttons {
   display: flex;
   flex-direction: row;
