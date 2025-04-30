d9144ab5a171a5d2c1264fd2de843c82b8dec842
Stefan Seemayer <stefan@seemayer.de>
2025-04-30 10:39:07 +0200

    feat: polish up start page UX

--- www/src/pages/index.vue
+++ www/src/pages/index.vue
@@ -6,14 +6,20 @@
         <q-card-section>
           <div class="text-h6">Open Database</div>
 
-          <q-file v-model="database" name="database" accept=".kdbx" label="Database" clearable filled class="q-mt-sm">
+          <q-file ref="$database" v-model="database" name="database" accept=".kdbx" label="Database" clearable filled
+            class="q-mt-sm">
             <template #prepend>
               <q-icon name="mdi-file-account" />
             </template>
           </q-file>
-          <q-input v-model="password" name="password" type="password" label="Password" clearable filled class="q-mt-sm">
+          <q-input ref="$password" v-model="password" name="password" :type="showPassword ? 'text' : 'password'"
+            label="Password" clearable filled class="password q-mt-sm">
             <template #prepend>
               <q-icon name="mdi-key-variant" />
+            </template>
+
+            <template #append>
+              <q-btn flat :icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" @click="showPassword = !showPassword" />
             </template>
           </q-input>
           <q-file v-model="keyFile" name="keyFile" label="Key file" clearable filled class="q-mt-sm">
@@ -25,11 +31,13 @@
 
         </q-card-section>
 
-        <q-card-actions align="right">
+        <q-card-actions>
+          <div class="bg-negative q-pa-sm q-ml-sm rounded-borders" v-if="errorMessage">{{ errorMessage }}</div>
+          <q-space />
           <q-btn-group>
-            <q-btn padding="sm lg" @click="onLoadExample">Load Example</q-btn>
-            <q-btn padding="sm lg" color="primary" type="submit">Open</q-btn>
-            <q-btn padding="sm lg">Reset</q-btn>
+            <q-btn padding="sm lg" :disabled="loading" @click="onLoadExample">Load Example</q-btn>
+            <q-btn padding="sm lg" :loading="loading" color="primary" type="submit">Open</q-btn>
+            <q-btn padding="sm lg" :disabled="loading">Reset</q-btn>
           </q-btn-group>
         </q-card-actions>
       </q-card>
@@ -51,11 +59,12 @@
 
 import demoDB from '@/assets/demo.kdbx?uint8array'
 
-import {ref} from 'vue'
+import {ref, useTemplateRef, watch} from 'vue'
 import {useRouter} from 'vue-router'
 
 import {useDatabasesStore} from '@/stores/databases'
 import {useAppStore} from '@/stores/app'
+import {QFile, QInput} from 'quasar'
 
 
 const app = useAppStore()
@@ -66,6 +75,23 @@ const database = ref(null)
 const password = ref("")
 const keyFile = ref(null)
 
+const loading = ref(false)
+const showPassword = ref(false)
+
+const errorMessage = ref("")
+
+const $database = useTemplateRef<QFile>("$database")
+const $password = useTemplateRef<QInput>("$password")
+
+watch(database, () => {
+  if (database.value && $password.value) {
+    $password.value.focus()
+  } else if (!database.value && $database.value) {
+    $database.value.focus()
+  }
+})
+
+setTimeout(() => {if ($database.value) {$database.value.focus()} }, 100)
 
 function readFile(file: File): Promise<Uint8Array> {
   return new Promise((resolve, reject) => {
@@ -88,22 +114,42 @@ function readFile(file: File): Promise<Uint8Array> {
 async function onSubmit() {
   if (!database.value) {return }
 
-  const databaseBuffer = await readFile(database.value)
-  const keyFileBuffer = keyFile.value ? await readFile(keyFile.value) : null
+  errorMessage.value = ""
+  loading.value = true
 
-  await databasesStore.loadDatabase(databaseBuffer, password.value, keyFileBuffer)
+  try {
 
-  await router.push({name: '/database/[i]/', params: {i: databasesStore.databases.length - 1}})
+    const databaseBuffer = await readFile(database.value)
+    const keyFileBuffer = keyFile.value ? await readFile(keyFile.value) : null
+
+    await databasesStore.loadDatabase(databaseBuffer, password.value, keyFileBuffer)
+
+    await router.push({name: '/database/[i]/', params: {i: databasesStore.databases.length - 1}})
+  } catch (e) {
+    if (typeof e === 'string') {
+      errorMessage.value = e
+    } else if (e instanceof Error) {
+      errorMessage.value = e.message
+    }
+  }
+
+  loading.value = false
+
 }
 
 async function onLoadExample() {
+  loading.value = true
   await databasesStore.loadDatabase(demoDB, "demopass", null)
   await router.push({name: '/database/[i]/', params: {i: databasesStore.databases.length - 1}})
-
+  loading.value = false
 }
 </script>
 
 <style lang="scss" scoped>
+.password {
+  font-family: monospace;
+}
+
 .q-footer a {
   color: inherit;
   text-decoration: none;
