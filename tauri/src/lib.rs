bea61555efc27922bd19101b34fb40248520198a
Stefan Seemayer <stefan@seemayer.de>
2025-05-03 21:56:49 +0200

    feat: backend code to do save-as with a dialog

--- tauri/src/lib.rs
+++ tauri/src/lib.rs
@@ -1,7 +1,8 @@
 use std::sync::Mutex;
 
 use omnikee_lib::{AppState, DatabaseOverview, Entry, OTPResponse};
-use tauri::Manager;
+use tauri::{AppHandle, Manager};
+use tauri_plugin_dialog::{DialogExt, FilePath};
 
 type State<'a> = tauri::State<'a, Mutex<AppState>>;
 
@@ -12,14 +13,51 @@ fn list_databases(state: State<'_>) -> Vec<DatabaseOverview> {
 }
 
 #[tauri::command]
-fn load_database(
+fn load_database_buffer(
     state: State<'_>,
     data: Vec<u8>,
     password: Option<String>,
     keyfile: Option<Vec<u8>>,
 ) -> Result<DatabaseOverview, String> {
     let mut state = state.lock().unwrap();
-    state.load_database(&data[..], password, keyfile)
+    state.load_database_buffer(&data[..], password, keyfile)
+}
+
+#[tauri::command]
+fn load_database_path(
+    state: State<'_>,
+    path: &str,
+    password: Option<String>,
+    keyfile: Option<Vec<u8>>,
+) -> Result<DatabaseOverview, String> {
+    let mut state = state.lock().unwrap();
+    state.load_database_path(path, password, keyfile)
+}
+
+#[tauri::command]
+fn save_database(state: State<'_>, database_idx: usize) -> Result<Option<Vec<u8>>, String> {
+    let mut state = state.lock().unwrap();
+    state.save_database(database_idx)
+}
+
+#[tauri::command]
+fn save_database_as(app: AppHandle, state: State<'_>, database_idx: usize) -> Result<(), String> {
+    let mut state = state.lock().unwrap();
+
+    let path = app
+        .dialog()
+        .file()
+        .add_filter("KeePass Databases", &["kdbx"])
+        .blocking_save_file()
+        .and_then(|p| p.into_path().ok());
+
+    if let Some(path) = path {
+        if let Some(path) = path.to_str() {
+            state.save_database_as(database_idx, path)?;
+        }
+    }
+
+    Ok(())
 }
 
 #[tauri::command]
@@ -64,7 +102,10 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             list_databases,
-            load_database,
+            load_database_buffer,
+            load_database_path,
+            save_database,
+            save_database_as,
             close_database,
             list_entries,
             reveal_protected,
